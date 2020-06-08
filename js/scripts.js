892329d13a08ab9d0d64b37faceadde733333e15
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-06-08 15:00:19 -0700

    add for loop to UI logic to populate storry
    Co-authored-by: Kelly Eidsvik <kelly.eidsvik@gmail.com>
    Co-authored-by: Ruthie Newman <ruthienewman@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,27 @@ $(document).ready(function() {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    const storyArray = [person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput];
+
+    storyArray.forEach(function(element) {
+      if (element === person1Input) {
+        $(".person1").text(person1Input);
+      } else if (element === person2Input) {
+        $(".person2").text(person2Input);
+      } else if (element === animalInput) {
+        $(".animal").append(animalInput);
+      } else if (element === exclamationInput) {
+        $(".exclamation").append(exclamationInput);
+      } else if (element === verbInput) {
+        $(".verb").append(verbInput);
+      } else if (element === nounInput) {
+        $(".noun").append(nounInput);
+      } 
+    });
 
     $("#story").show();
 
     event.preventDefault();
+ 
   });
-});
+}); 
